55b10b3449acb9dd39110fb45538e9e383e5b7a3
Beytullah KALAY <beytullah.kalay@outlook.com>
2023-11-29 17:36:03 +0300

    chore: change folder path for EnemyMotorcycleFlyingCar

---
+++ Assets/_Scripts/CarControllers/EnemyMotorcycleFlyingCar.cs
@@ -0,0 +1,20 @@
+using Zenject;
+
+public class EnemyMotorcycleFlyingCar : FlyingCar
+{
+
+    [Inject]
+    private void Constructor(CarSettings.EnemyMotorcycleSetting carSettings)
+    {
+        var settings = carSettings.settings;
+        Multiplier = settings.MoveSettings.Multiplier;
+        MoveForce = settings.MoveSettings.MoveForce;
+        TurnTorque = settings.MoveSettings.TurnForce;
+        FlowingDistance = settings.FlowingSettings.FlowingDistance;
+        FlowingFrequency = settings.FlowingSettings.FlowingFrequency;
+        StabilizationLerpForce = settings.StabillizationSettings.StabilizationLerpForce;
+        WhatIsGround = settings.StabillizationSettings.WhatIsGround;
+        MinCrashForce = settings.CrashSettings.MinCrashForce;
+    }
+    
+}
